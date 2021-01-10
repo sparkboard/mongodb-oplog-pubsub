a52d43139cbaa186ebb92c101b026b89dde2fb5f
Matthew Huebert <mhuebert@gmail.com>
2021-01-10 11:14:00 +0100

    do not use noCursorTimeout

--- src/vendor/mongo-oplog/src/stream.js
+++ src/vendor/mongo-oplog/src/stream.js
@@ -30,7 +30,7 @@ module.exports = async ({ db, ns, ts, coll }) => {
     tailable: true,
     awaitData: true,
     oplogReplay: true,
-    noCursorTimeout: true,
+    // noCursorTimeout: true,
     numberOfRetries: Number.MAX_VALUE
   })).stream()
 }
